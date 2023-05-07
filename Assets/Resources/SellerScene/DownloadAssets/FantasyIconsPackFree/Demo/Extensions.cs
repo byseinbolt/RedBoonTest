fba9c5c1caebfa10528c57ceca93068c822c4807
byseinbolt <busovitor@gmail.com>
2023-05-07 14:41:18 +0300

    create sellerScene folder and move all resources

---
+++ Assets/Resources/SellerScene/DownloadAssets/FantasyIconsPackFree/Demo/Extensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Resources.DownloadAssets.FantasyIconsPackFree.Demo
+{
+    public static class Extensions
+    {
+        public static List<T> Shuffle<T>(this List<T> source)
+        {
+            source.Sort((i, j) => Random.Range(0, 3) - 1);
+
+            return source;
+        }
+    }
+}
